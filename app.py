e01258e6c5f48b4bd9231f69cb1d755e02c76b55
schlys <samchlystek20@gmail.com>
2023-02-07 11:31:06 -0500

    display titles, ingredient, and instructions

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ es.initialize()
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        result = speech_to_text()
-        es.search(' '.join(result))
-        return render_template("list.html", items=result)
+        # result = speech_to_text()
+        # recipes = es.search(' '.join(result))
+        recipes = es.search('green onion pepper')
+        print(recipes)
+        return render_template("list.html", items=recipes)
     return render_template("webpage.html")
 
 if __name__ == '__main__':
